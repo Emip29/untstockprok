939542deff011acdbdac5d994d5c7bace222112d
Emip29 <emiliopalacio@my.unt.edu>
2025-11-16 20:07:46 -0600

    Update main.py

--- main.py
+++ main.py
@@ -91,7 +91,7 @@ with fundamental_tab:
     st.header("Fundamental Data")
 
     try:
-        key = "XCWQ3FD4VCKVL1NA"
+        key = "G8MVGNKCB35F0RKF"
         fd = FundamentalData(key, output_format="pandas")
         st.subheader("Balance Sheet (Annual)")
         bs = fd.get_balance_sheet_annual(ticker)[0]
